9d7cc6015e68592ac1fdc0d90135b0a3946c35c2
Chan Woo <14cpark@gmail.com>
2020-02-01 11:41:08 -0800

    Instantiate FileRepository in MainActivityViewModel

--- app/src/main/java/com/parkchanwoo/tamagotchiar/viewmodels/MainActivityViewModel.java
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/viewmodels/MainActivityViewModel.java
@@ -5,8 +5,14 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.parkchanwoo.tamagotchiar.repositories.FileRepository;
+
 public class MainActivityViewModel extends AndroidViewModel {
+	private String TAG = this.getClass().getSimpleName();
+	private FileRepository fileRepository;
+
 	public MainActivityViewModel(@NonNull Application application) {
 		super(application);
+		fileRepository = new FileRepository(application);
 	}
 }
