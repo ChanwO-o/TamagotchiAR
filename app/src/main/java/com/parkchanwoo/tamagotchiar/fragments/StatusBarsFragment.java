fd6c457fcde2723083a9c86a443411ff9ed31e4e
Chan Woo <14cpark@gmail.com>
2020-02-01 20:56:42 -0800

    Move StatusBarsFragment into fragments package

--- app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/StatusBarsFragment.java
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/StatusBarsFragment.java
@@ -1,4 +1,4 @@
-package com.parkchanwoo.tamagotchiar;
+package com.parkchanwoo.tamagotchiar.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parkchanwoo.tamagotchiar.R;
 
 public class StatusBarsFragment extends Fragment {
 		// Required empty public constructor
