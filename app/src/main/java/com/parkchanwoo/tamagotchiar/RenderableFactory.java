a35c269173c53e175068ff903b308acb66dd6777
chanwoo <14cpark@gmail.com>
2020-02-02 04:59:05 -0800

    Implement buildRenderable() in RenderableFactory

---
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/RenderableFactory.java
@@ -0,0 +1,17 @@
+package com.parkchanwoo.tamagotchiar;
+
+import android.content.Context;
+
+import com.google.ar.sceneform.rendering.ModelRenderable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RenderableFactory {
+
+	public CompletableFuture<ModelRenderable> buildRenderable(Context context, int resource) {
+		//Build the ModelRenderable
+		return ModelRenderable.builder()
+				.setSource(context, R.raw.dog1)
+				.build();
+	}
+}
