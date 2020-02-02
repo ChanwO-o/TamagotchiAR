f151009b1339af39ea7a5c6b8aaa62cb95ad1cb5
Minhduc Cao <minhduc.t.cao@gmail.com>
2020-02-01 18:32:19 -0800

    Added status bars for hunger, happiness, bathroom

---
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/StatusBarsFragment.java
@@ -0,0 +1,27 @@
+package com.parkchanwoo.tamagotchiar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StatusBarsFragment extends Fragment {
+		// Required empty public constructor
+	public StatusBarsFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_status_bars, container, false);
+	}
+}
