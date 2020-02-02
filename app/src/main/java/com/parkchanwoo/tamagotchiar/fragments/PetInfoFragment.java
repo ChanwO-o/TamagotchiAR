db65c03bb823ee83d925c6062479a68b42cb4cb1
Chan Woo <14cpark@gmail.com>
2020-02-01 20:04:12 -0800

    Create Pet object from PetInfoFragment inputs, pass to ViewModel

--- app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/PetInfoFragment.java
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/PetInfoFragment.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.parkchanwoo.tamagotchiar.Pet;
 import com.parkchanwoo.tamagotchiar.R;
 import com.parkchanwoo.tamagotchiar.viewmodels.MainActivityViewModel;
+
+import java.util.Date;
 
 public class PetInfoFragment extends Fragment {
 	private String TAG = this.getClass().getSimpleName();
 	private MainActivityViewModel mainActivityViewModel;
+	private EditText etPetInfoName, etPetInfoDOB;
+	private RadioButton rbPetInfoMale, rbPetInfoFemale;
+	private Button bPetInfoCreatePet;
 
 	public PetInfoFragment() {
 
@@ -25,8 +34,14 @@ public class PetInfoFragment extends Fragment {
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		View v = inflater.inflate(R.layout.fragment_petinfo, container, false);
-
-
+		etPetInfoName = v.findViewById(R.id.etPetInfoName);
+		etPetInfoDOB = v.findViewById(R.id.etPetInfoDOB);
+		rbPetInfoMale = v.findViewById(R.id.rbPetInfoMale);
+		rbPetInfoFemale = v.findViewById(R.id.rbPetInfoFemale);
+		bPetInfoCreatePet = v.findViewById(R.id.bPetInfoCreatePet);
+		bPetInfoCreatePet.setOnClickListener(view -> {
+			createPet();
+		});
 		return v;
 	}
 
@@ -34,5 +49,17 @@ public class PetInfoFragment extends Fragment {
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		mainActivityViewModel = ViewModelProviders.of(getActivity()).get(MainActivityViewModel.class);
+	}
+
+	private void createPet() {
+		String name = etPetInfoName.getText().toString();
+		String dob = etPetInfoDOB.getText().toString();
+		String gender;
+		if (rbPetInfoMale.isChecked())
+			gender = "male";
+		else
+			gender = "female";
+		Pet pet = new Pet(name, new Date(dob), gender);
+		mainActivityViewModel.setPetLiveData(pet);
 	}
 }
