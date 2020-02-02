8b77c01a38bcda49842ab06c5446a2e74ef06594
chanwoo <14cpark@gmail.com>
2020-02-02 04:55:06 -0800

    Render a confetti model

--- app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/TamagotchiARFragment.java
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/TamagotchiARFragment.java
@@ -11,6 +11,7 @@ import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
@@ -20,7 +21,8 @@ import com.parkchanwoo.tamagotchiar.viewmodels.MainActivityViewModel;
 public class TamagotchiARFragment extends ArFragment {
 	private String TAG = this.getClass().getSimpleName();
 	private MainActivityViewModel mainActivityViewModel;
-	private ModelRenderable androidRenderable;
+	private ModelRenderable petRenderable, confettiRenderable;
+	private AnchorNode anchorNode;
 	private boolean petCreated = false;
 
 	@Override
@@ -38,7 +40,17 @@ public class TamagotchiARFragment extends ArFragment {
 		ModelRenderable.builder()
 				.setSource(getContext(), R.raw.dog1)
 				.build()
-				.thenAccept(renderable -> androidRenderable = renderable)
+				.thenAccept(renderable -> petRenderable = renderable)
+				.exceptionally(
+						throwable -> {
+							Log.e(TAG, "Unable to load renderable");
+							return null;
+						});
+
+		ModelRenderable.builder()
+				.setSource(getContext(), R.raw.confetti)
+				.build()
+				.thenAccept(renderable -> confettiRenderable = renderable)
 				.exceptionally(
 						throwable -> {
 							Log.e(TAG, "Unable to load renderable");
@@ -48,19 +60,27 @@ public class TamagotchiARFragment extends ArFragment {
 		//Listen for onTap events
 		setOnTapArPlaneListener(
 				(HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-					if (androidRenderable == null || petCreated)
+					if (petRenderable == null || petCreated)
 						return;
 					Anchor anchor = hitResult.createAnchor();
-					AnchorNode anchorNode = new AnchorNode(anchor); //Build a node of type AnchorNode
+					anchorNode = new AnchorNode(anchor); //Build a node of type AnchorNode
 					anchorNode.setParent(getArSceneView().getScene()); //Connect the AnchorNode to the Scene
 					TransformableNode transformableNode = new TransformableNode(getTransformationSystem()); //Build a node of type TransformableNode
 					transformableNode.setParent(anchorNode); //Connect the TransformableNode to the AnchorNode
-					transformableNode.setRenderable(androidRenderable); //Attach the Renderable
+					transformableNode.setRenderable(petRenderable); //Attach the Renderable
 					transformableNode.select(); //Set the node
 					transformableNode.getScaleController().setMinScale(0.30f);
 					transformableNode.getScaleController().setMaxScale(0.35f);
 					petCreated = true; // prevent user from creating multiple pets
+					confetti();
 				}
 		);
+	}
+
+	private void confetti() {
+		TransformableNode confettiNode = new TransformableNode(getTransformationSystem());
+		confettiNode.setLocalScale(new Vector3(0.1f, 0.1f, 0.1f));
+		confettiNode.setParent(anchorNode);
+		confettiNode.setRenderable(confettiRenderable);
 	}
 }
