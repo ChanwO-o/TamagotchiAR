77d3673b2a0d8f047f498b43fe7de1501428d962
Chan Woo <14cpark@gmail.com>
2020-02-02 01:20:15 -0800

    Observe Pet stats and display on ProgressBars

--- app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/StatusBarsFragment.java
+++ app/src/main/java/com/parkchanwoo/tamagotchiar/fragments/StatusBarsFragment.java
@@ -2,16 +2,25 @@ package com.parkchanwoo.tamagotchiar.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.parkchanwoo.tamagotchiar.Pet;
 import com.parkchanwoo.tamagotchiar.R;
+import com.parkchanwoo.tamagotchiar.viewmodels.MainActivityViewModel;
 
 public class StatusBarsFragment extends Fragment {
-		// Required empty public constructor
+	private String TAG = this.getClass().getSimpleName();
+	private MainActivityViewModel mainActivityViewModel;
+	private ProgressBar hungerBar, happinessBar, bathroomBar;
+
 	public StatusBarsFragment() {
 	}
 
@@ -23,7 +32,22 @@ public class StatusBarsFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_status_bars, container, false);
+		View v = inflater.inflate(R.layout.fragment_status_bars, container, false);
+		hungerBar = v.findViewById(R.id.HungerBar_ProgBar);
+		happinessBar = v.findViewById(R.id.HappinessBar_ProgBar);
+		bathroomBar = v.findViewById(R.id.BathroomBar_ProgBar);
+		return v;
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mainActivityViewModel = ViewModelProviders.of(getActivity()).get(MainActivityViewModel.class);
+		LiveData<Pet> petLiveData = mainActivityViewModel.getPetLiveData();
+		petLiveData.observe(getViewLifecycleOwner(), pet -> {
+			hungerBar.setProgress(pet.getHunger());
+			happinessBar.setProgress(pet.getHappiness());
+			bathroomBar.setProgress(pet.getBathroom());
+		});
 	}
 }
